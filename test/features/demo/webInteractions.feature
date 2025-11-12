5f8bf3cc76cbddfa6b985b917d3a23f67685632f
rusteex11Js <vinothjsautomation@gmail.com>
2025-11-12 20:51:55 +0530

    added web intergartion

--- test/features/demo/webInteractions.feature
+++ test/features/demo/webInteractions.feature
@@ -1,6 +1,5 @@
 Feature: Web Interactions
 
-    @ttest
     Scenario Outline: Demo Web Interactions
         Given A Web page is opened <url>
         When perform web interactions
@@ -8,3 +7,35 @@ Feature: Web Interactions
         Examples:
             | TestID     | url     |
             | DEMO_TC002 | /inputs |
+
+    Scenario Outline: Demo Web drop down Interactions
+        Given A Web page is opened <url>
+        When perform web interactions drop down
+
+        Examples:
+            | TestID     | url       |
+            | DEMO_TC002 | /dropdown |
+
+    Scenario Outline: Demo Web check box
+        Given A Web page is opened <url>
+        When perform web interactions checkboxes
+
+        Examples:
+            | TestID     | url         |
+            | DEMO_TC002 | /checkboxes |
+
+    Scenario Outline: Demo Web handling windows
+        Given A Web page is opened <url>
+        When perform web interactions handling windows
+
+        Examples:
+            | TestID     | url      |
+            | DEMO_TC002 | /windows |
+
+    Scenario Outline: Demo Web handling Alerts
+        Given A Web page is opened <url>
+        When perform web interactions handling windows
+
+        Examples:
+            | TestID     | url      |
+            | DEMO_TC002 | /windows |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -66,4 +66,124 @@ Then(/^perform web interactions$/, async function () {
     }
 })
 
+Then(/^perform web interactions drop down$/, async function () {
+    /** 
+     * Drop down:
+     * Actions:
+     * Assert default option is selected
+     * Select by attribute, text, index
+     * get a list of options
+    */
+
+    await browser.pause(2000);
+
+    /**
+     * Validate the selected value
+     */
+    // let element =  $(`//select/option[@selected="selected"]`)
+    // let value = await element.getText();
+    // console.log(value)
+    // expect(value).to.equal("Please select an option")
+
+    //select a specific option
+    // let ddEle = $(`#dropdown`)
+    // await ddEle.selectByVisibleText("Option 1");
+
+    // await browser.pause(2000);
+    // await ddEle.selectByAttribute("value","2");
+
+    // await browser.pause(2000);
+    // await ddEle.selectByIndex(1);
+
+    /**
+     * Get list of options in the drop down
+     */
+
+    let eleArr = $$(`select[id='dropdown'] > option`)
+    let size = await eleArr.length;
+    let arr =[];
+
+   for(let i = 0; i < size; i++){
+    let ele = eleArr[i];
+    let val = ele.getText();
+    arr.push(val);
+   }
+
+   console.log(`>> Options Array: ${arr} `)
+    
+})
+
+When(/^perform web interactions checkboxes$/, async function () {
+    /** 
+     *check boxes:
+     * Actions:
+     * Assert default option is selected
+     * Select by attribute, text, index
+     * get a list of options
+    */
+
+    await browser.pause(2000);
+
+    let element = $(`//form[@id="checkboxes"]/input[1]`)
+    let isChecked = await element.isSelected();
+    expect(isChecked).to.true
+  
+})
+
+
+When(/^perform web interactions handling windows$/, async function () {
+    /** 
+     *4.Windows handling
+     * steps:
+     * 1.launch the browser
+     * 2.open another windows
+     * 3.switch to the window based on title
+     * 4.switch backt to the main window
+     * 
+     * 
+     * Methods used:
+     * 1.getTitle()
+     * 2.getWindowHandle()
+     * 3.getWindowHandles()
+     * 4.switchToWindow()
+    */
+
+   await browser.pause(2000)
+
+   let element1 = $(`=Click Here`)
+   let element2 = $(`=Elemental Selenium`)
+
+   await element1.click();
+   await element2.click();
+
+   let currentWindowTitle =  await browser.getTitle();
+   let parentWinHandle = await browser.getWindowHandle();
+   console.log(`current window Title: ${currentWindowTitle}`)
+
+   //switch to specifi window
+   let winHandles =  await browser.getWindowHandles();
+   for(let i = 0; i < winHandles.length; i++){
+      console.log(`>> win handle: ${winHandles[i]}`);
+
+      await browser.switchToWindow(winHandles[i])
+      currentWindowTitle = await browser.getTitle();
+
+      if(currentWindowTitle === "Home | Elemental Selenium"){
+           await browser.switchToWindow(winHandles[i])
+           let headerTextEleSel = await $(`<h1>`).getText();
+           console.log(`headerTextEleSel: ${headerTextEleSel}`)
+           // do business logic
+           break;
+      }
+   }
+
+   //Switch back to parent window
+
+   await browser.switchToWindow(parentWinHandle);
+   let parentWinHeaderText = await $(`<h3>`).getText();
+
+   console.log(`>> parent window header text: ${parentWinHeaderText}`)
+   // continue with rest of the execution
+   
+})
 
